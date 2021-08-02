a89136a2d9ddbb5cd98b6ba47dec5ccb62f82b56
sabohat <sabohat2802@gmail.com>
2021-08-02 15:31:32 +0500

    add:login page style

--- components/login/LoginForm.js
+++ components/login/LoginForm.js
@@ -4,38 +4,38 @@ const LoginInput = () => {
   return (
     <div className="flex flex-col">
       <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-        <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+        <div className="bg-white px-6 py-8 rounded shadow-md border-2 border-green-100 text-black w-full">
           <h1 className="mb-8 text-3xl text-center">Sign up</h1>
           <input
             type="text"
-            className="outline-none border w-full p-3 border-yellow-200 rounded mb-4 focus:border-green-300"
+            className="input-field mb-4"
             name="fullname"
             placeholder="Full Name"
           />
 
           <input
             type="text"
-            className="outline-none block border border-grey-light w-full p-3 rounded mb-4"
+            className="input-field mb-4"
             name="email"
             placeholder="Email"
           />
 
           <input
             type="password"
-            className=" p-3 mb-4"
+            className="input-field mb-4"
             name="password"
             placeholder="Password"
           />
           <input
             type="password"
-            className="input-field p-3 mb-4"
+            className="input-field mb-4"
             name="confirm_password"
             placeholder="Confirm Password"
           />
 
           <button
             type="submit"
-            className="w-full text-center py-3 rounded bg-green-500 text-white hover:bg-green-dark focus:outline-none my-1"
+            className="button-basic my-1"
           >
             Create Account
           </button>
